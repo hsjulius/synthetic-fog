24159c339daa892d736cffd51dd3298aea927a2e
hsjulius <60826854+hsjulius@users.noreply.github.com>
2023-12-04 22:54:04 -0500

    merge

--- code/main.py
+++ code/main.py
@@ -1,48 +1,110 @@
-import argparse
-import os
-import sys
-import csv
-import numpy as np
-import matplotlib.pyplot as plt
+# import argparse
+# import os
+# import sys
+# import csv
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import cv2
+
+
+# """
+
+# We def ne the input photograph, the radiance attenuation
+# ratio map and the volumetric scattering radiance map as the
+# Ref ection Map, the Transmittance Map and the Volumetric Map respectively
+
+# input photograph = reflection map 
+# radiance attenuation ratio map = transmittance map 
+# volumetric scattering radiance map = volumetric map 
+
+# """
+
+
+# # Load the input image
+# # input_image_path = 'your_image.jpg'
+# # image = Image.open(input_image_path)
+
+# # Perform automatic color correction to estimate illumination
+
+# def illuminationEstimation():
+#     print("not implemented yet")
+
+# def geometryEstimation():
+#     print("not implemented yet")
+
+# def transmittanceMap():
+#     print("not implemented yet")
+
+# def volumetricMap():
+#     print("not implemented yet")
+
+# def main():
+#     file = "1.png"
+#     img = cv2.imread(f"../data/{file}", )
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+#     reflectionMap = img
+#     # corrected_image = automatic_color_correction(img)
+#     # img.show(title='Original Image')
+#     # corrected_image.show(title='Illumination Corrected Image')
+
+
+    
+#     plt.imshow(img)
+#     plt.show()
+
+
+# main()
+
+
+
+
 import cv2
+import numpy as np
 
+def compute_dark_channel(image, window_size=15):
+    # Compute the dark channel of the image
+    min_channel = np.min(image, axis=2)
+    dark_channel = cv2.erode(min_channel, np.ones((window_size, window_size)))
 
-"""
+    return dark_channel
 
-We def ne the input photograph, the radiance attenuation
-ratio map and the volumetric scattering radiance map as the
-Ref ection Map, the Transmittance Map and the Volumetric Map respectively
+def compute_atmospheric_light(image, dark_channel):
+    # Select the top 0.1% brightest pixels from the dark channel
+    num_pixels = dark_channel.size
+    num_brightest = int(0.001 * num_pixels)
+    indices = np.argpartition(dark_channel.flatten(), -num_brightest)[-num_brightest:]
 
-input photograph = reflection map 
-radiance attenuation ratio map = transmittance map 
-volumetric scattering radiance map = volumetric map 
+    # Find the maximum intensity value in the original image at these pixels
+    atmospheric_light = np.max(image.reshape(-1, 3)[indices], axis=0)
 
-"""
+    return atmospheric_light
 
+def compute_transmission(image, atmospheric_light, omega=0.95, t0=0.1):
+    # Compute the transmission map using the haze model
+    transmission = 1 - omega * compute_dark_channel(image / atmospheric_light)
 
-def illuminationEstimation():
-    print("not implemented yet")
+    # Clamp the values to be between t0 and 1
+    transmission = np.maximum(transmission, t0)
 
-def geometryEstimation():
-    print("not implemented yet")
+    return transmission
 
-def transmittanceMap():
-    print("not implemented yet")
+# Load the input image
+image_path = '../data/1.png'
+image = cv2.imread(image_path) / 255.0  # Normalize to [0, 1]
 
-def volumetricMap():
-    print("not implemented yet")
+# Compute the dark channel
+dark_channel = compute_dark_channel(image)
 
-def main():
-    file = "1.png"
-    img = cv2.imread(f"../data/{file}", )
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# Compute the atmospheric light
+atmospheric_light = compute_atmospheric_light(image, dark_channel)
 
-    reflectionMap = img
-    
-    plt.imshow(img)
-    plt.show()
+# Compute the transmission map
+transmission_map = compute_transmission(image, atmospheric_light)
 
-    
-
-
-main()
+# Display the results
+cv2.imshow('Original Image', cv2.cvtColor((image * 255).astype(np.uint8), cv2.COLOR_BGR2RGB))
+cv2.imshow('Transmission Map', (transmission_map * 255).astype(np.uint8))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
